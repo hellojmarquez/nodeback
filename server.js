eea1771e03697bddd1a97d511c7effa193f57df5
javier marquez <hello.jmarquez@gmail.com>
2022-10-28 10:29:50 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.get('/api', (req, res) => {
+	// res.writeHead(200, { 'Content-type': 'text/html' });
+	res.json({ message: 'Hola desde el servidor!' });
+
+	res.end();
+});
+app.post('/post', (req, res) => {
+	if (req.method == 'POST') {
+		console.log('conectado');
+		// res.writeHead(200, { 'Content-type': 'text/html' });
+		res.json({ message: 'Hola desde el servidor!' });
+		// let f = console.log('conectado al server');
+		// res.end(f);
+	}
+	if (req.method == 'GET') res.end('metodo get');
+});
+
+app.listen(4000);
+console.log('corriendo server');
