2e7129ce093210fea0ac7dd608a8279e65bce0bb
javier marquez <hello.jmarquez@gmail.com>
2022-10-28 10:44:20 -0500

    package arreglado

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/api', (req, res) => {
 
 	res.end();
 });
-app.post('/post', (req, res) => {
+app.post('https://nodetest-production-e09b.up.railway.app/post', (req, res) => {
 	if (req.method == 'POST') {
 		console.log('conectado');
 		// res.writeHead(200, { 'Content-type': 'text/html' });
